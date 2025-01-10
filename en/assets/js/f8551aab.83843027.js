2ab1d68bfcf943ce842c848cd598fff066003343
pk <pk>
2025-01-10 11:50:00 +0000

    Update site

---
+++ en/assets/js/f8551aab.83843027.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpersonal_site=self.webpackChunkpersonal_site||[]).push([[2224],{4575:(e,s,n)=>{n.r(s),n.d(s,{default:()=>r});n(6540);var i=n(1410),t=n(797),c=n(4848);function r(){const{siteConfig:e}=(0,t.A)();return(0,c.jsxs)(i.A,{children:[(0,c.jsx)("h1",{children:"My React page"}),(0,c.jsx)("p",{children:e.customFields.description})]})}}}]);
